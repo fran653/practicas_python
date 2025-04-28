cd26f2b5572fd178074e4ab78e330621d72aaa5e
Topot <fran653@hotmail.com>
2025-04-28 22:13:39 +0200

    varios

--- all_checks.py
+++ all_checks.py
@@ -1,14 +1,26 @@
-#!/usr/bin/env python3
-  
-import os
-import sys
+#!/usr/binenv python3
+#atom all_checks.py
+(...)
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
 
-def main():
+def main(): 
     if check_reboot():
         print("Pending Reboot.")
+        sys_exit(1)
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
+        print("Disk full.")
         sys.exit(1)
-
-    print("Everything ok.")
+    
+    print("Everything ok")
     sys.exit(0)
 
 main()
