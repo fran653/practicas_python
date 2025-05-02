6ae32116db569b5f66fccc78795cbfed7e0b459f
Topot <fran653@hotmail.com>
2025-05-02 21:12:37 +0200

    más tests de unittest

--- rearrange_test.py
+++ rearrange_test.py
@@ -2,10 +2,26 @@
 import unittest
 from rearrange import rearrange_name
 class TestRearrange(unittest.TestCase):
+  
   def test_basic(self):
     testcase = "Lovelace, Ada"
     expected = "Ada Lovelace"
     self.assertEqual(rearrange_name(testcase), expected)
 
-'''chmod +x rearrange_test.py 
-./rearrange_test.py '''
+  def test_empty(self):
+    testcase = ""
+    expected = ""
+    self.assertEqual(rearrange_name(testcase), expected)
+
+  def test_double_name(self):
+    testcase = "Hopper, Grace M."
+    expected = "Grace M. Hopper"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+  def test_one_name(self):
+    testcase = "Voltaire"
+    expected = "Voltaire"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+# Run the tests - batería con todos juntos
+unittest.main()
