60ddd83d863eb119a8e86b4b957ce0a4e2b770aa
Topot <fran653@hotmail.com>
2025-05-10 12:21:23 +0200

    pruebas con bash

--- bashcommands.sh
+++ bashcommands.sh
@@ -1,7 +1,8 @@
-echo "Starting at: $(date)"; echo
-echo "UPTIME"; uptime; echo
-echo "FREE"; free; echo
-echo "WHO"; who; echo
+line="----------------------------"
+echo "Starting at: $(date)"; echo $line
+echo "UPTIME"; uptime; echo $line
+echo "FREE"; free; echo $line
+echo "WHO"; who; echo $line
 echo "Finishing at: $(date)"
 
 
